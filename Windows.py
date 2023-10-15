ddb7c029c5fcf0cf256f8fd861615b3274f07e53
Lernt (Georgii Zautashvili) <lernt33@gmail.com>
2023-10-16 00:04:13 +0400

    Initial commit

---
+++ Windows.py
@@ -0,0 +1,21 @@
+import webbrowser, time
+import os
+print("@Lernt33's first script")
+
+a = input("GitHub nickname: ")
+url = "github.com/"+ a +"/"
+
+while True:
+ try:
+  views = int(input("Views quantity: "))
+  break
+ except:
+  print("\nThat's not a number!\n")
+
+for i in range(views):
+ webbrowser.open_new(url)
+ time.sleep(0.5)
+
+time.sleep(3)
+os.system("taskkill /im msedge.exe /f")
+webbrowser.open_new("https://"+url)
---
+++ readme.txt
@@ -0,0 +1,3 @@
+@Lernt33 - Just a simple python script, for increasing views on GitHub
+For Linux u have to write "chmod +x Linux.sh" in terminal,in correct directory
+
